72cebaac4c22eaedc123c4c9210a82272fa61172
Alisa Bakovic <alisabakovic@Alisas-MacBook-Air.local>
2025-11-19 11:25:13 +0100

    added formating to the HTML

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,38 +6,37 @@ with open("animals_template.html", "r") as f:
 
 def load_data(file_path):
 
-
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animal_list = load_data('animals_data.json')
 
-output = ''
+animal_list = load_data("animals_data.json")
+
+output = ""
 
 for animal in animal_list:
     name = animal["name"]
     diet = animal["characteristics"].get("diet")
-    location = animal["locations"][0]  if animal["locations"] else None
+    location = animal["locations"][0] if animal["locations"] else None
     type_animal = animal["characteristics"].get("type")
 
     output += '<li class="cards__item">'
 
-
-    output += f"Name: {name}<br/>\n"
+    output += f' <div class="card__title">{name}</div>\n'
+    output += ' <p class="card__text">\n'
 
     if diet:
-        output += f"Diet: {diet}<br/>\n"
+        output += f" <strong>Diet:</strong> {diet}<br/>\n"
     if location:
-        output += f"Location: {location}<br/>\n"
+        output += f" <strong>Location:</strong> {location}<br/>\n"
     if type_animal:
-        output += f"Type: {type_animal}<br/>\n"
+        output += f" <strong>Type:</strong> {type_animal}<br/>\n"
 
-
-    output += '</li>\n\n'
+    output += "</p>\n"
+    output += "</li>\n\n"
 
 
 new_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
 
 with open("animals.html", "w") as f:
     f.write(new_html)
-
