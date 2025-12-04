13855be5bcab7b271e1d61183a51ff2f92ef9383
Alisa Bakovic <alisabakovic@Alisas-MacBook-Air.local>
2025-12-04 09:48:20 +0100

    Updated API_KEY and API_URL to use uppercase constants

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,16 +3,16 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv("API_KEY")
+API_KEY = os.getenv("API_KEY")
 
-url = "https://api.api-ninjas.com/v1/animals"
+URL = "https://api.api-ninjas.com/v1/animals"
 
 def fetch_data(animal_name):
 
-    headers = {"X-Api-Key": api_key}
+    headers = {"X-Api-Key": API_KEY}
     params = {"name": animal_name}
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(URL, headers=headers, params=params)
 
     if response.status_code == 200:
         return response.json()
